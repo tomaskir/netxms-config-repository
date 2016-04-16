ab14ff12f93c35615a71a01fdad82d9d1978f43f
Tomas Kirnak <tomas@atris.sk>
2016-04-16 16:01:02 +0200

    More formatting fixes

--- src/main/java/sk/atris/netxms/confrepo/Application.java
+++ src/main/java/sk/atris/netxms/confrepo/Application.java
@@ -3,13 +3,26 @@ package sk.atris.netxms.confrepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Properties;
-
 @SpringBootApplication
 public class Application {
-	public static void main(String[] args) {
-        System.setProperty("spring.main.banner-mode", "off");
+    public static void main(String[] args) {
+        final String springBanner = "off";
+        final String databasePath = System.getProperty("database.file.path");
+        final String databaseUser = System.getProperty("database.user");
+        final String databasePass = System.getProperty("database.pass");
+        final String databaseUrl = "jdbc:hsqldb:file:" + databasePath;
 
-		SpringApplication.run(Application.class, args);
-	}
+        System.out.println(databaseUrl);
+        System.out.println(databaseUser);
+        System.out.println(databasePass);
+
+        // configure Spring properties
+        System.setProperty("spring.main.banner-mode", springBanner);
+        System.setProperty("spring.datasource.url", databaseUrl);
+        System.setProperty("spring.datasource.username", databaseUser);
+        System.setProperty("spring.datasource.password", databasePass);
+
+        // run the application
+        SpringApplication.run(Application.class, args);
+    }
 }
