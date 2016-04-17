9a1c179983e929f64fda2d1b1f3ed6963c6e958c
Tomas Kirnak <tomas@atris.sk>
2016-04-18 00:03:44 +0200

    Database configuration loaded on application startup

--- src/main/java/sk/atris/netxms/confrepo/Application.java
+++ src/main/java/sk/atris/netxms/confrepo/Application.java
@@ -5,22 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    private static final String springBannerMode = "off";
+
     public static void main(String[] args) {
-        final String springBanner = "off";
-        final String databasePath = System.getProperty("database.file.path");
-        final String databaseUser = System.getProperty("database.user");
-        final String databasePass = System.getProperty("database.pass");
-        final String databaseUrl = "jdbc:hsqldb:file:" + databasePath;
-
-        System.out.println(databaseUrl);
-        System.out.println(databaseUser);
-        System.out.println(databasePass);
-
         // configure Spring properties
-        System.setProperty("spring.main.banner-mode", springBanner);
-        System.setProperty("spring.datasource.url", databaseUrl);
-        System.setProperty("spring.datasource.username", databaseUser);
-        System.setProperty("spring.datasource.password", databasePass);
+        System.setProperty("spring.main.banner-mode", springBannerMode);
 
         // run the application
         SpringApplication.run(Application.class, args);
---
+++ src/main/java/sk/atris/netxms/confrepo/data/DatabaseConfiguration.java
@@ -0,0 +1,34 @@
+package sk.atris.netxms.confrepo.data;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+    @Value("${database.file.path}")
+    private String dbFilePath;
+
+    @Value("${database.user}")
+    private String dbUser;
+
+    @Value("${database.pass}")
+    private String dbPass;
+
+    @Bean
+    public DataSource configureDatabase() {
+        // TODO: fail if DB connection info not configured in application.properties
+
+        final String databaseUrl = "jdbc:hsqldb:file:" + dbFilePath;
+
+        return DataSourceBuilder
+                .create()
+                .url(databaseUrl)
+                .username(dbUser)
+                .password(dbPass)
+                .build();
+    }
+}
