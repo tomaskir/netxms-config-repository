88ff7059872f28f1ee375812bee5a0daec22c9c1
Tomas Kirnak <tomas@unimus.net>
2017-03-06 14:51:27 +0100

    Improved configuration loading at startup

--- src/main/java/sk/atris/netxms/confrepo/Application.java
+++ src/main/java/sk/atris/netxms/confrepo/Application.java
@@ -13,6 +13,11 @@ import java.io.InputStream;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
+    /**
+     * .jar entry point
+     *
+     * @param args application startup arguments
+     */
     public static void main(String[] args) {
         configureApplicationProfiles("jar");
         loadApplicationConfiguration();
@@ -21,6 +26,12 @@ public class Application extends SpringBootServletInitializer {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * .war entry point
+     *
+     * @param application provided by Spring
+     * @return application startup class
+     */
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         configureApplicationProfiles("war");
@@ -29,6 +40,14 @@ public class Application extends SpringBootServletInitializer {
         return application.sources(Application.class);
     }
 
+    /**
+     * Configures active Spring profiles.
+     * <p>
+     * {@code jar} or {@code war} depending on running application type.<br>
+     * {@code win} or {@code unix} depending on platform application is running on.
+     *
+     * @param runType application type
+     */
     private static void configureApplicationProfiles(String runType) {
         String profiles = runType;
 
@@ -43,47 +62,56 @@ public class Application extends SpringBootServletInitializer {
         System.setProperty("spring.profiles.active", profiles);
     }
 
+    /**
+     * Loads the application configuration.
+     * <p>
+     * If {@code application.properties} is present on the classpath, its loaded from there (in dev environment).<br>
+     * {@code application.properties} file is not packaged in the final product.
+     * <p>
+     * In the final product, the config is loaded @{symmetry.conf}.<br>
+     * The file location depends on the OS on which the application is deployed.
+     */
     private static void loadApplicationConfiguration() {
         final String propertiesFileName = "netxms-config-repository.conf";
 
-        if (Application.class.getClassLoader().getResource(propertiesFileName) != null) {
-            // if we have application.properties on the classpath, load from there
-            System.out.println("Loading '" + propertiesFileName + "' from classpath");
-
-            InputStream propertiesStream = Application.class.getClassLoader().getResourceAsStream(propertiesFileName);
-            if (propertiesStream == null) {
-                System.out.println("Configuration file '" + propertiesFileName + "' not found on classpath; application shutting down!");
-                throw new IllegalStateException("Failed to read application configuration");
-            }
-
-            loadProperties(propertiesStream, "classpath");
-
-        } else {
-            // otherwise, we load application.properties from external file
-            final String propertiesFilePath;
-
-            if (isWindows()) {
-                propertiesFilePath = "C:\\ProgramData\\" + propertiesFileName;
-            } else {
-                propertiesFilePath = "/etc/" + propertiesFileName;
-            }
-
-            System.out.println("Loading properties file '" + propertiesFilePath + "'");
-
-            InputStream propertiesStream;
-            try {
-                propertiesStream = new FileInputStream(propertiesFilePath);
-
-            } catch (FileNotFoundException e) {
-                System.out.println("Configuration file '" + propertiesFilePath + "' not found; application shutting down!");
-                throw new IllegalStateException("Failed to read application configuration");
-
-            }
-
-            loadProperties(propertiesStream, "'" + propertiesFilePath + "'");
+        // if we have application.properties on the classpath, Spring will automatically load those
+        if (Application.class.getClassLoader().getResource("application.properties") != null) {
+            System.out.println("Loading 'application.properties' from classpath");
+            return;
         }
+
+        // otherwise, we load external configuration file
+        final String propertiesFilePath;
+
+        if (isWindows()) {
+            propertiesFilePath = "C:\\ProgramData\\" + propertiesFileName;
+        } else {
+            propertiesFilePath = "/etc/" + propertiesFileName;
+        }
+
+        System.out.println("Loading configuration file '" + propertiesFilePath + "'");
+
+        // using try-with-resources, InputStream will be closed automatically after try block
+        try (InputStream propertiesStream = new FileInputStream(propertiesFilePath)) {
+            System.getProperties().load(propertiesStream);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Configuration file '" + propertiesFilePath + "' not found; application shutting down!");
+            throw new IllegalStateException("Failed to read application configuration");
+
+        } catch (IOException e) {
+            System.out.println("Failed to read configuration file from '" + propertiesFilePath + "'; application shutting down!");
+            throw new IllegalStateException("Failed to read application configuration");
+
+        }
+
     }
 
+    /**
+     * Returns if current platform is Windows.
+     *
+     * @return {@code true} or {@code false} if running on Windows
+     */
     private static boolean isWindows() {
         String osName = System.getProperty("os.name").toLowerCase();
 
@@ -92,25 +120,6 @@ public class Application extends SpringBootServletInitializer {
         } else {
             return false;
         }
-    }
-
-    private static void loadProperties(InputStream propertiesStream, String filePathDescription) {
-        try {
-            System.getProperties().load(propertiesStream);
-
-        } catch (IOException e) {
-            System.out.println("Failed to read configuration file from " + filePathDescription + "; application shutting down!");
-            throw new IllegalStateException("Failed to read application configuration");
-
-        } finally {
-            if (propertiesStream != null) {
-                try {
-                    propertiesStream.close();
-                } catch (IOException ignored) {
-                }
-            }
-        }
-
     }
 
 }
